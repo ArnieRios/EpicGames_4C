b22e1a8108886f81c57d5df5b6f80929b94372fe
Grupo Proyecto <arnierios361@gmail.com>
2026-02-18 00:08:44 -0500

    Se pudo probar con éxito el método validarAcceso de LoginController

--- src/main/java/com/epicgames/app/controller/LoginController.java
+++ src/main/java/com/epicgames/app/controller/LoginController.java
@@ -1,55 +1,59 @@
 package com.epicgames.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.epicgames.app.model.UsuarioDTO;
+import com.epicgames.app.repository.IUsuarioRepository;
 
 @Controller
 @RequestMapping("/epicgames")
-public class LoginController {
-	@GetMapping("/principal")
-	public String abrirPrincipal() {
-		return "principal";
-	}
+public class LoginController { 
 	
 	@GetMapping("/login")
 	public String abrirLogin() {
 		return "login";
 	}
 	
+	@Autowired
+	private IUsuarioRepository repoUsu;
+	@PostMapping("/login")
+	public String validarAcceso(
+			@RequestParam("email") String usuario,
+			@RequestParam("clave") String clave,
+			Model model) {
+		UsuarioDTO u = repoUsu.findByEmailAndClave(usuario, clave);
+		System.out.println("Buscando a: " + usuario + " con clave: " + clave);
+		if (u != null) {
+			System.out.println("DEBUG: Usuario encontrado: " + u.getNombre());
+			model.addAttribute("mensaje", "Bienvenido: " + u.getNombre());
+			model.addAttribute("cssmensaje", "alert alert-success");
+			return "principal";
+		} else {
+			System.out.println("DEBUG: No se encontró el usuario en la base de datos.");
+			model.addAttribute("mensaje", "Usuario o clave incorrecto");
+			model.addAttribute("cssmensaje", "alert alert-danger");
+			return "login";
+		}		
+	}
+	
+			
+	
+	
 	@GetMapping("/crearcuenta")
 	public String abrirCrearCuenta() {
 		return "crearcuenta";
 	}
-	
-	@GetMapping("/mantenimiento")
-	public String abrirMantenimiento() {
-		return "mantenimientojuegos";
-	}
-	
+		
 	@GetMapping("/actualizarcuenta") 
 	public String abrirActualizarCuenta() {
 		return "actualizarcuenta";
 	}
 	
-	@GetMapping("/actualizarjuego")
-	public String abrirActualizarJuego() {
-		return "actualizarjuegos";
-	}
-	
-	@GetMapping("/canasta")
-	public String abrirCanasta() {
-		return "canasta";
-	}
-	
-	@GetMapping("/catalogo")
-	public String abrirCatalogo() {
-		return "catalogo";
-	}
-	
-	@GetMapping("/compra")
-	public String abrirCompra() {
-		return "compra";
-	}
 
 }
